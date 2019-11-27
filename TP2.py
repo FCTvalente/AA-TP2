1cd6e159887a738686b96c6f5c9153fa241e6b5e
Pedro <pd.valente@campus.fct.unl.pt>
2019-11-27 03:25:53 +0000

    feature selection 1

--- TP2.py
+++ TP2.py
@@ -4,6 +4,7 @@ import numpy as np
 from tp2_aux import images_as_matrix, report_clusters
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE, Isomap
+from sklearn.feature_selection import f_classif
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.metrics import silhouette_score, adjusted_rand_score
 
@@ -27,11 +28,18 @@ except IOError:
 
     np.savez('feature_res.npz', pca_data=pca_data, tsne_data=tsne_data, iso_data=iso_data)
 
-#data_labels = np.load('labels.txt')
+data_labels = np.loadtxt('labels.txt', delimiter=',')
+#stacked_features = np.concatenate((pca_data, tsne_data, iso_data), axis=1)
+
+#labeled = data_labels[data_labels[:,1]>0,:]
+#print(pca_data[data_labels[:,1]>0,:])
+pca_f, pca_prob = f_classif(pca_data[data_labels[:,1]>0,:], data_labels[data_labels[:,1]>0,1])
+tsne_f, tsne_prob = f_classif(tsne_data[data_labels[:,1]>0,:], data_labels[data_labels[:,1]>0,1])
+iso_f, iso_prob = f_classif(iso_data[data_labels[:,1]>0,:], data_labels[data_labels[:,1]>0,1])
 
 kmeans = KMeans(n_clusters=4)
 lbls = kmeans.fit_predict(pca_data)
-report_clusters(np.linspace(0, 562, 563), lbls, 'teste.html')
+report_clusters(np.linspace(0, data_labels.shape[0] - 1, data_labels.shape[0]), lbls, 'teste.html')
 
 
 
